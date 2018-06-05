be5b5f99c1052d8a126fa68fd5be40cb934537eb
Jie Xue <xuejie328@gmail.com>
2018-06-04 22:14:54 -0400

    Add files via upload

---
+++ Dash/Code/Install_package.py
@@ -0,0 +1,9 @@
+import pip
+pip.main(['install','dash'])
+pip.main(['install','dash-renderer'])
+pip.main(['install','dash-html-components'])
+pip.main(['install','dash-core-components'])
+pip.main(['install','plotly'])
+
+pip.main(['install', '--upgrade','plotly'])
+pip.main(['install', '--upgrade','pip'])
