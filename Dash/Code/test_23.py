a0d916db029be661ada286d63afe6b0090b5d094
Jie Xue <xuejie328@gmail.com>
2018-07-02 13:34:45 -0400

    Add files via upload

--- Dash/Code/test_23.py
+++ Dash/Code/test_23.py
@@ -19,12 +19,14 @@ import os
 import base64
 import io
 
-
+from collections import namedtuple
 
 app = dash.Dash()
 
 # Set base case
-df_base = pd.read_excel('C:\\Users\\c257458\\Desktop\\Artificial pancreas review\\Insulin Reservoir\\Capacitive Sensor\\Data\\2018 May 29 Tue\\FDC1004\\Test1\\14_52-no-noise-on-cloth-labcoat.xlsx', usecols = [0,3,4,5])
+df_base = pd.read_excel(
+    'C:\\Users\\c257458\\Desktop\\Artificial pancreas review\\Insulin Reservoir\\Capacitive Sensor\\Data\\2018 May 29 Tue\\FDC1004\\Test1\\14_52-no-noise-on-cloth-labcoat.xlsx',
+    usecols=[0, 3, 4, 5])
 df_base.columns = ['t', 'Volume', 'Sensor', 'Ref']
 df_base['round t'] = df_base['t'].apply(round, ndigits=0)
 df_base = df_base.groupby('round t').mean().reset_index()
@@ -34,39 +36,42 @@ df_base_Spline = df_base.groupby('Volume').mean().reset_index().sort_values(by=[
 spl = UnivariateSpline(df_base_Spline['Volume'], df_base_Spline['Sensor'])
 spl.set_smoothing_factor(0.34)
 
+
 # Noise remove
-def noiseRemoval(data):
+def noise_removal(data):
     ref_max = 0.9000520706176756
     ref_min = 0.7775897979736323
     sense_max = 0.7217592511504041
     sense_min = -0.00010679833326410204
     k = 0.55880274
-    
-    data['Ref_MinMax'] =  (data['Ref'] - ref_min) / (ref_max - ref_min)
-    adjust = (data['Ref_MinMax']*k)*(sense_max - sense_min) + sense_min
+
+    data['Ref_MinMax'] = (data['Ref'] - ref_min) / (ref_max - ref_min)
+    adjust = (data['Ref_MinMax'] * k) * (sense_max - sense_min) + sense_min
     return data['Sensor'] - adjust
-    
-def noiseRemovalSample(obs,ref):
+
+
+def noise_removal_sample(obs, ref):
     ref_max = 0.9000520706176756
     ref_min = 0.7775897979736323
     sense_max = 0.7217592511504041
     sense_min = -0.00010679833326410204
     k = 0.55880274
-    
-    Ref_MinMax = (ref - ref_min) / (ref_max - ref_min)
-    adjust = (Ref_MinMax*k)*(sense_max - sense_min) + sense_min
-    
+
+    ref_minmax = (ref - ref_min) / (ref_max - ref_min)
+    adjust = (ref_minmax * k) * (sense_max - sense_min) + sense_min
+
     return obs - adjust
+
 
 # Sample
 Volume_True = np.arange(3100, 400, -100)
-itr = np.arange(1,len(Volume_True)+1,1)
-LookUp_Volume = np.arange(0,3500,1)
+itr = np.arange(1, len(Volume_True) + 1, 1)
+LookUp_Volume = np.arange(0, 3500, 1)
 LookUp_pf = spl(LookUp_Volume)
 pf_True = spl(Volume_True)
 
 
-def find_Obs(data, value):
+def find_obs(data, value):
     # Find obs
     idx = (np.abs(data['Volume'] - value)).idxmin()
     return data['Sensor'][idx]
@@ -79,16 +84,20 @@ def find_ref(data, value):
 
 
 # Kalman filter
-from collections import namedtuple
 gaussian = namedtuple('Gaussian', ['mean', 'var'])
-## Prediction
-def predict(Volume, d):
-    return gaussian(Volume.mean - d, Volume.var)
-## Update
+
+
+def predict(volume, d):
+    # Prediction
+    return gaussian(volume.mean - d, volume.var)
+
+
 def gaussian_multiply(g1, g2):
+    # Update
     mean = (g1.var * g2.mean + g2.var * g1.mean) / (g1.var + g2.var)
     variance = (g1.var * g2.var) / (g1.var + g2.var)
     return gaussian(mean, variance)
+
 
 def update(prior, likelihood):
     posterior = gaussian_multiply(likelihood, prior)
@@ -121,46 +130,44 @@ app.layout = html.Div([
         multiple=False),
     html.Div(id='intermediate-value', style={'display': 'none'}),
 
-    
     html.Button(
         id='submit-button',
         n_clicks=0,
         children='Submit',
         style={'fontSize': 28}
     ),
-    
+
     html.Br(),
     html.Div([
         html.H5("Experimental data"),
         html.Div(dte.DataTable(rows=[{}], id='table'))
-        ]),
-    
+    ]),
+
     html.Br(),
     html.Div([
         html.H5("Down-Sampled data"),
         html.Div(dte.DataTable(rows=[{}], id='table-ds'))
-        ]),
-    
-    # Data explore 
+    ]),
+
+    # Data explore
     html.Br(),
     html.Div(children=[
         html.Div(dcc.Graph(id='graph-1'), className="six columns"),
         html.Div(dcc.Graph(id='graph-2'), className="six columns"),
-        ], style={'width': '100%', 'display': 'inline-block'}),
+    ], style={'width': '100%', 'display': 'inline-block'}),
 
-        
-    # Data reduce noise     
+    # Data reduce noise
     html.Br(),
     html.Div([
         dcc.Graph(id='graph-3')
-        ]),
-        
-    # Data sample        
+    ]),
+
+    # Data sample
     html.Br(),
     html.Div([
         dcc.Graph(id='graph-4')
-        ]),
-           
+    ]),
+
 ])
 
 
@@ -174,11 +181,11 @@ def parse_contents(contents, filename):
     try:
         if 'csv' in filename:
             # Assume that the user uploaded a CSV file
-            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')), usecols = [0,3,4,5])
+            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')), usecols=[0, 3, 4, 5])
             df.columns = ['t', 'Volume', 'Sensor', 'Ref']
         elif 'xls' in filename:
             # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded), usecols = [0,3,4,5])
+            df = pd.read_excel(io.BytesIO(decoded), usecols=[0, 3, 4, 5])
             df.columns = ['t', 'Volume', 'Sensor', 'Ref']
     except Exception as e:
         print(e)
@@ -202,13 +209,14 @@ def update_output_1(contents, filename):
         return [{}]
 
 
-# Define downSample function
-def downSample(data):
+# Define down_sample function
+def down_sample(data):
     data['round t'] = data['t'].apply(round, ndigits=0)
     data = data.groupby('round t').mean().reset_index()
     data = data.drop('t', axis=1)
-    
+
     return data
+
 
 # callback Storing Data in the Browser with a Hidden Div
 @app.callback(Output('intermediate-value', 'children'),
@@ -218,12 +226,13 @@ def clean_data(contents, filename):
     if contents is not None:
         df = parse_contents(contents, filename)
         if df is not None:
-            df = downSample(df)
+            df = down_sample(df)
             return df.to_json(date_format='iso', orient='split')
 
 
-# callback table creation (downsampled data)
-@app.callback(Output('table-ds', 'rows'), [Input('submit-button', 'n_clicks')], [State('intermediate-value', 'children')])
+# callback table creation (down_sampled data)
+@app.callback(Output('table-ds', 'rows'), [Input('submit-button', 'n_clicks')],
+              [State('intermediate-value', 'children')])
 def update_table(n_clicks, jsonified_cleaned_data):
     if jsonified_cleaned_data is not None:
         dff = pd.read_json(jsonified_cleaned_data, orient='split')
@@ -232,231 +241,237 @@ def update_table(n_clicks, jsonified_cleaned_data):
         return [{}]
 
 
-# callback figure creation (downsampled data explore 1)
-@app.callback(Output('graph-1', 'figure'), [Input('submit-button', 'n_clicks')], [State('intermediate-value', 'children')])
+# callback figure creation (down_sampled data explore 1)
+@app.callback(Output('graph-1', 'figure'), [Input('submit-button', 'n_clicks')],
+              [State('intermediate-value', 'children')])
 def update_graph_1(n_clicks, jsonified_cleaned_data):
     if jsonified_cleaned_data is not None:
         dff = pd.read_json(jsonified_cleaned_data, orient='split')
         traces = []
         traces.append(go.Scatter(
-            x = dff['round t'],
-            y = dff['Sensor'],
-            mode = 'markers',
+            x=dff['round t'],
+            y=dff['Sensor'],
+            mode='markers',
             marker={'size': 3},
-            name = 'Capacitance sensor')
-            )
+            name='Capacitance sensor')
+        )
         traces.append(go.Scatter(
-            x = dff['round t'],
-            y = dff['Ref'],
-            mode = 'markers',
+            x=dff['round t'],
+            y=dff['Ref'],
+            mode='markers',
             marker={'size': 3},
-            name = 'Reference sensor')
-            )
-    
-        return{
+            name='Reference sensor')
+        )
+
+        return {
             'data': traces,
             'layout': go.Layout(
-                title = 'Sensor Measurement vs. Time',
-                xaxis = dict(title = 'Time (s)'),
-                yaxis = dict(title = 'Capacitance (pF)'),
-                legend=dict(orientation="h",xanchor="center",x=0.5,y=1.1)
+                title='Sensor Measurement vs. Time',
+                xaxis=dict(title='Time (s)'),
+                yaxis=dict(title='Capacitance (pF)'),
+                legend=dict(orientation="h", xanchor="center", x=0.5, y=1.1)
             )
         }
     else:
         return {'data': []}
 
 
-# callback figure creation (downsampled data explore2)
-@app.callback(Output('graph-2', 'figure'), [Input('submit-button', 'n_clicks')], [State('intermediate-value', 'children')])
+# callback figure creation (down_sampled data explore2)
+@app.callback(Output('graph-2', 'figure'), [Input('submit-button', 'n_clicks')],
+              [State('intermediate-value', 'children')])
 def update_graph_2(n_clicks, jsonified_cleaned_data):
     if jsonified_cleaned_data is not None:
-        dff = pd.read_json(jsonified_cleaned_data, orient='split')        
-        df = pd.DataFrame(columns=['text1','text2'])
+        dff = pd.read_json(jsonified_cleaned_data, orient='split')
+        df = pd.DataFrame(columns=['text1', 'text2'])
         df['text1'] = pd.Series(dff['Sensor'].round(2), dtype=str)
-        df['text2'] = pd.Series(dff['Volume'].round(2), dtype=str)+"uL, "+df['text1']+"pF"
+        df['text2'] = pd.Series(dff['Volume'].round(2), dtype=str) + "uL, " + df['text1'] + "pF"
         traces = []
         traces.append(go.Scatter(
-            x = dff['Volume'],
-            y = dff['Sensor'],
-            hoverinfo = "text",
-            text = df['text2'],
-            mode = 'markers',
-            name = 'Noisy data',
+            x=dff['Volume'],
+            y=dff['Sensor'],
+            hoverinfo="text",
+            text=df['text2'],
+            mode='markers',
+            name='Noisy data',
             marker={'size': 3}
-            )
+        )
         )
         traces.append(go.Scatter(
-            x = df_base['Volume'],
-            y = df_base['Sensor'],
-            mode = 'markers',
-            name = 'No noise data',
+            x=df_base['Volume'],
+            y=df_base['Sensor'],
+            mode='markers',
+            name='No noise data',
             marker={'size': 3}
-            )
+        )
         )
         traces.append(go.Scatter(
-            x = df_base['Volume'],
-            y = spl(df_base['Volume']),
-            name = 'Actual value (Spline)',
-            line = dict(
-                color = ('rgb(0, 0, 0)'),
-                width = 2,
+            x=df_base['Volume'],
+            y=spl(df_base['Volume']),
+            name='Actual value (Spline)',
+            line=dict(
+                color='rgb(0, 0, 0)',
+                width=2,
                 dash='dash')
-            )
+        )
         )
 
-        return{
+        return {
             'data': traces,
             'layout': go.Layout(
-                title = 'Sensor Measurement vs. Volume',
-                xaxis = dict(title = 'Volume in reservior (uL)'),
-                yaxis = dict(title = 'Capacitance (pF)'),
-                legend=dict(orientation="h",xanchor="center",x=0.5,y=1.1)
+                title='Sensor Measurement vs. Volume',
+                xaxis=dict(title='Volume in reservior (uL)'),
+                yaxis=dict(title='Capacitance (pF)'),
+                legend=dict(orientation="h", xanchor="center", x=0.5, y=1.1)
             )
         }
     else:
-        return {'data': []}    
-        
+        return {'data': []}
 
-# callback figure creation (Noise removal data)
-@app.callback(Output('graph-3', 'figure'), [Input('submit-button', 'n_clicks')], [State('intermediate-value', 'children')])
+    # callback figure creation (Noise removal data)
+
+
+@app.callback(Output('graph-3', 'figure'), [Input('submit-button', 'n_clicks')],
+              [State('intermediate-value', 'children')])
 def update_graph_3(n_clicks, jsonified_cleaned_data):
     if jsonified_cleaned_data is not None:
-        dff = pd.read_json(jsonified_cleaned_data, orient='split')        
+        dff = pd.read_json(jsonified_cleaned_data, orient='split')
         traces = []
         traces.append(go.Scatter(
-            x = dff['Volume'],
-            y = dff['Sensor'],
-            mode = 'markers',
-            name = 'Noisy data',
+            x=dff['Volume'],
+            y=dff['Sensor'],
+            mode='markers',
+            name='Noisy data',
             marker={'size': 3}
-            )
+        )
         )
         traces.append(go.Scatter(
-            x = df_base['Volume'],
-            y = df_base['Sensor'],
-            mode = 'markers',
-            name = 'No noise data',
+            x=df_base['Volume'],
+            y=df_base['Sensor'],
+            mode='markers',
+            name='No noise data',
             marker={'size': 3}
-            )
+        )
         )
         traces.append(go.Scatter(
-            x = df_base['Volume'],
-            y = spl(df_base['Volume']),
-            name = 'Actual value (Spline)',
-            line = dict(
-                color = ('rgb(0, 0, 0)'),
-                width = 2,
+            x=df_base['Volume'],
+            y=spl(df_base['Volume']),
+            name='Actual value (Spline)',
+            line=dict(
+                color='rgb(0, 0, 0)',
+                width=2,
                 dash='dash')
-            )
+        )
         )
         traces.append(go.Scatter(
-            x = dff['Volume'],
-            y = noiseRemoval(dff),
-            mode = 'markers',
-            name = 'Noise reduced data',
+            x=dff['Volume'],
+            y=noise_removal(dff),
+            mode='markers',
+            name='Noise reduced data',
             marker={'size': 3}
-            )
+        )
         )
 
-        return{
+        return {
             'data': traces,
             'layout': go.Layout(
-                title = 'Noise Reduction Comparison',
-                xaxis = dict(title = 'Volume in reservior (uL)'),
-                yaxis = dict(title = 'Capacitance (pF)'),
-                hovermode = 'closest',
-                legend=dict(orientation="v",xanchor="center",x=0.1,y=1)
+                title='Noise Reduction Comparison',
+                xaxis=dict(title='Volume in reservior (uL)'),
+                yaxis=dict(title='Capacitance (pF)'),
+                hovermode='closest',
+                legend=dict(orientation="v", xanchor="center", x=0.1, y=1)
             )
         }
     else:
-        return {'data': []}    
-        
-# callback figure creation (Sample data)
-@app.callback(Output('graph-4', 'figure'), [Input('submit-button', 'n_clicks')], [State('intermediate-value', 'children')])
+        return {'data': []}
+
+    # callback figure creation (Sample data)
+
+
+@app.callback(Output('graph-4', 'figure'), [Input('submit-button', 'n_clicks')],
+              [State('intermediate-value', 'children')])
 def update_graph_4(n_clicks, jsonified_cleaned_data):
     if jsonified_cleaned_data is not None:
-        dff = pd.read_json(jsonified_cleaned_data, orient='split')   
+        dff = pd.read_json(jsonified_cleaned_data, orient='split')
         dff_1 = dff.copy()
-        pf_Obs = [find_Obs(dff_1, x) for x in Volume_True]
+        pf_obs = [find_obs(dff_1, x) for x in Volume_True]
         dff_2 = dff.copy()
-        pf_Ref = [find_ref(dff_2, x) for x in Volume_True]
-        
+        pf_ref = [find_ref(dff_2, x) for x in Volume_True]
+
         adjust_obs = []
-        for i in np.arange(len(pf_Obs)):
-            adjust_obs.append(noiseRemovalSample(pf_Obs[i],pf_Ref[i]))
-        
+        for i in np.arange(len(pf_obs)):
+            adjust_obs.append(noise_removal_sample(pf_obs[i], pf_ref[i]))
+
         traces = []
         # Obs Data
         traces.append(go.Scatter(
-            x = dff['Volume'],
-            y = dff['Sensor'],
-            mode = 'markers',
-            name = 'Observed value',
-            marker={'size': 3, 'color':('rgb(70, 130, 180)')}
-            )
+            x=dff['Volume'],
+            y=dff['Sensor'],
+            mode='markers',
+            name='Observed value',
+            marker={'size': 3, 'color': 'rgb(70, 130, 180)'}
+        )
         )
         traces.append(go.Scatter(
-            x = Volume_True,
-            y = pf_Obs,
-            mode = 'markers',
-            name = 'Observed value (Sample)',
-            marker={'size': 10, 'color':('rgb(70, 130, 180)')}
-            )
+            x=Volume_True,
+            y=pf_obs,
+            mode='markers',
+            name='Observed value (Sample)',
+            marker={'size': 10, 'color': 'rgb(70, 130, 180)'}
         )
-        
-        # Adjusted Data
-        traces.append(go.Scatter(
-            x = dff['Volume'],
-            y = noiseRemoval(dff),
-            mode = 'markers',
-            name = 'Noise reduced data',
-            marker={'size': 3, 'color':('rgb(11, 120, 35)')}
-            )
-        )
-        traces.append(go.Scatter(
-            x = Volume_True,
-            y = adjust_obs,
-            mode = 'markers',
-            name = 'Noise reduced data',
-            marker={'size': 10, 'color':('rgb(11, 120, 35)')}
-            )
-        )
-        
-        # Actual Data
-        traces.append(go.Scatter(
-            x = Volume_True,
-            y = pf_True,
-            mode = 'markers',
-            name = 'Actual value (Sample)',
-            marker={'size': 10, 'color':('rgb(0, 0, 0)')}
-            )
-        )
-        traces.append(go.Scatter(
-            x = df_base['Volume'],
-            y = spl(df_base['Volume']),
-            name = 'Actual value (Spline)',
-            line = dict(
-                color = ('rgb(0, 0, 0)'),
-                width = 2,
-                dash='dash')
-            )
         )
 
-        return{
+        # Adjusted Data
+        traces.append(go.Scatter(
+            x=dff['Volume'],
+            y=noise_removal(dff),
+            mode='markers',
+            name='Noise reduced data',
+            marker={'size': 3, 'color': 'rgb(11, 120, 35)'}
+        )
+        )
+        traces.append(go.Scatter(
+            x=Volume_True,
+            y=adjust_obs,
+            mode='markers',
+            name='Noise reduced data',
+            marker={'size': 10, 'color': 'rgb(11, 120, 35)'}
+        )
+        )
+
+        # Actual Data
+        traces.append(go.Scatter(
+            x=Volume_True,
+            y=pf_True,
+            mode='markers',
+            name='Actual value (Sample)',
+            marker={'size': 10, 'color': 'rgb(0, 0, 0)'}
+        )
+        )
+        traces.append(go.Scatter(
+            x=df_base['Volume'],
+            y=spl(df_base['Volume']),
+            name='Actual value (Spline)',
+            line=dict(
+                color='rgb(0, 0, 0)',
+                width=2,
+                dash='dash')
+        )
+        )
+
+        return {
             'data': traces,
             'layout': go.Layout(
-                title = 'Noise Reduction Comparison',
-                xaxis = dict(title = 'Volume in reservior (uL)'),
-                yaxis = dict(title = 'Capacitance (pF)'),
-                hovermode = 'closest',
-                legend=dict(orientation="v",xanchor="center",x=0.1,y=1)
+                title='Noise Reduction Comparison',
+                xaxis=dict(title='Volume in reservoir (uL)'),
+                yaxis=dict(title='Capacitance (pF)'),
+                hovermode='closest',
+                legend=dict(orientation="v", xanchor="center", x=0.1, y=1)
             )
         }
     else:
-        return {'data': []}    
+        return {'data': []}
 
 
-        
 app.css.append_css({
     "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
 })
